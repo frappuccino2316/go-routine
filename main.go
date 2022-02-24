edda8cc012e5b85f7f050611a14a15cf5f8be80c
Frappuccino001 <fumi00hiro@icloud.com>
2022-02-24 12:02:28 +0900

    routine

--- main.go
+++ main.go
@@ -7,21 +7,26 @@ import (
 
 func main() {
 	ch := make(chan int)
+	ch2 := make(chan int)
 
 	go receiver(ch)
+	go receiver(ch2)
 
-	i := 0
-	for i < 100 {
-		ch <- i
-		i++
+	fmt.Println("first")
+	for i := range ch {
+		fmt.Println(i)
+	}
+
+	fmt.Println("second")
+	for i := range ch2 {
+		fmt.Println(i)
 	}
 
 	time.Sleep(time.Second)
 }
 
 func receiver(ch chan int) {
-	for {
-		i := <-ch
-		fmt.Println(i)
+	for i := 0; i < 10; i++ {
+		ch <- i
 	}
 }
